9488b37cfecec281965e7624b31bb9176bc0d101
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2020-02-19 21:44:53 -0500

    Changed the work directories to previous folders "selection experiment"

--- scripts/1_tribulus_predation_analysis.R
+++ scripts/1_tribulus_predation_analysis.R
@@ -9,7 +9,7 @@
 # NOTES: 
 ##########################################################################################
 
-tribulus_data = read.csv("data/tribulus_combined.csv")
+tribulus_data = read.csv("selection experiment/point in time data/processed/tribulus_combined.csv")
 head(tribulus_data)
 tribulus_data[which(is.na(tribulus_data$eaten.insects)),"eaten.insects"]<-0
 
@@ -23,7 +23,7 @@ length(which(!is.na(tribulus_data$eaten)))
 tables.eat= table(tribulus_data$eaten.insects,tribulus_data$eaten)
 lgth_insc_eaten =length(which(!is.na(tribulus_data$eaten.insects)))
 
-png("output/Proportion_of_seeds_predation.png", height = 5, width = 5,res = 300, units = "in")
+png("selection experiment/output/Proportion_of_seeds_predation.png", height = 5, width = 5,res = 300, units = "in")
 barplot(round(tables.eat/lgth_insc_eaten,3)*100, 
         main = "Barplot birds and insect predation", 
         ylim = c(0,100),
