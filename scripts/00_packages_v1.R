046bc9cf1136766e8673ed6ec1259c3fcbec8071
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2021-10-04 15:52:07 -0400

    R scripts selection experiment

---
+++ scripts/00_packages_v1.R
@@ -0,0 +1,10 @@
+#################################################################
+################Tribulus Selection experiments Dataset##############
+#################################################################
+### Goal: Compare the two methods of natural selection estimates. Point in time and Mark Recapture.
+### By Daniel Reyes Corral
+### 
+### Package script
+
+library(readr)
+library(tidyverse)
