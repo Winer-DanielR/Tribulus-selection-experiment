be204501d7a19d10f122f179ebf79c174d9accce
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2022-06-13 18:27:11 -0500

    Created a new script for the trait comparison plot. It uses the trait dataset

--- scripts/09_PT _Trait comparisons_plot.R
+++ scripts/09_PT _Trait comparisons_plot.R
@@ -2,6 +2,37 @@
 # By: Daniel Reyes Corral
 # Date: 6/13/2022
 
+length_depth <- left_join(length_pivot, depth_pivot)
+length_depth <- na.omit(length_depth)
+
+
+ggplot(length_depth) +
+  aes(
+    x = length_mean,
+    y = depth_mean,
+    group = population,
+    colour = population,
+    shape = survival
+  ) +
+  geom_point(size = 3L, show.legend = T) +
+  geom_path(aes(group = population), 
+            color = "black", 
+            arrow = arrow(length = unit(0.3,"cm")), size = 0.7) +
+  #scale_color_brewer(palette = "Dark2") +
+  labs(
+    x = "Length (mm)",
+    y = "Depth (mm)",
+    title = "Length - Depth Selection Estimates Plot",
+    caption = "Means per Population,
+    per Island or all mericarps and survived mericarps",
+    color = "Populations",
+    shape = "Survival"
+  ) +
+  #geom_text_repel(aes(label = population)) +
+  ggthemes::theme_few() + 
+  facet_grid(rows = vars(island), cols = vars(year),
+             scales = "free")
+
 # Length - Depth comparisons per Island ####
 length_depth %>%
   filter(island %in% "Santa.Cruz") %>%
