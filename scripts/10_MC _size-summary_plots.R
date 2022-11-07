ccfa926e5dc59122fffdbc2e0d271cb6d26209d6
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2022-11-07 09:29:39 -0500

    Added figure merge for all plots.

--- scripts/10_MC _size-summary_plots.R
+++ scripts/10_MC _size-summary_plots.R
@@ -12,7 +12,7 @@ library(dplyr)
 library(ggplot2)
 
 # Floreana 2018 size plot ####
-MC_Floreana_18 %>%
+Floreana18 <- MC_Floreana_18 %>%
  filter(time %in% "0") %>%
  ggplot() +
  aes(x = size, y = length) +
@@ -28,7 +28,7 @@ MC_Floreana_18 %>%
       panel.background = element_rect(fill = NA))
 
 # Isabela 2018 size plot ####
-MC_Isabela_18 %>%
+Isabela18 <- MC_Isabela_18 %>%
   filter(time %in% "0") %>%
   ggplot() +
   aes(x = size, y = length) +
@@ -44,7 +44,7 @@ MC_Isabela_18 %>%
         panel.background = element_rect(fill = NA))
 
 # Santa Cruz 2018 ####
-MC_StCruz_18 %>%
+Santa_cruz18 <- MC_StCruz_18 %>%
   filter(time %in% "0") %>%
   ggplot() +
   aes(x = size, y = length) +
@@ -59,3 +59,68 @@ MC_StCruz_18 %>%
         text = element_text(family = "Noto Sans"),
         panel.background = element_rect(fill = NA))
 
+# Floreana 2019 ####
+Floreana19 <- MC_Floreana_19 %>%
+  filter(time %in% "0") %>%
+  ggplot() +
+  aes(x = size, y = length) +
+  geom_boxplot(fill = "#E69F00", size = 1) +
+  labs(x = "Mericarp Size", 
+       y = "Length (mm)", title = "Floreana 2019") +
+  theme(axis.line = element_line(linetype = "solid", size = 1.5), 
+        axis.title = element_text(size = 14, face = "bold", family = "Noto Sans"), 
+        axis.text = element_text(size = 14, family = "Noto Sans"),
+        axis.text.x = element_text(size = 12), 
+        plot.title = element_text(size = 16, family = "Noto Sans", face = "bold"),
+        text = element_text(family = "Noto Sans"),
+        panel.background = element_rect(fill = NA))
+
+# Isabela 2019 ####
+Isabela19 <- MC_Isabela_19 %>%
+  filter(time %in% "0") %>%
+  ggplot() +
+  aes(x = size, y = length) +
+  geom_boxplot(fill = "#56B4E9", size = 1) +
+  labs(x = "Mericarp Size", 
+       y = "Length (mm)", title = "Isabela 2019") +
+  theme(axis.line = element_line(linetype = "solid", size = 1.5), 
+        axis.title = element_text(size = 14, face = "bold", family = "Noto Sans"), 
+        axis.text = element_text(size = 14, family = "Noto Sans"),
+        axis.text.x = element_text(size = 12), 
+        plot.title = element_text(size = 16, family = "Noto Sans", face = "bold"),
+        text = element_text(family = "Noto Sans"),
+        panel.background = element_rect(fill = NA))
+
+# Santa Cruz 2019 ####
+Santa_cruz19 <- MC_StCruz_19 %>%
+  filter(time %in% "0") %>%
+  ggplot() +
+  aes(x = size, y = length) +
+  geom_boxplot(fill = "#009E73", size = 1) +
+  labs(x = "Mericarp Size", 
+       y = "Length (mm)", title = "Santa Cruz 2019") +
+  theme(axis.line = element_line(linetype = "solid", size = 1.5), 
+        axis.title = element_text(size = 14, face = "bold", family = "Noto Sans"), 
+        axis.text = element_text(size = 14, family = "Noto Sans"),
+        axis.text.x = element_text(size = 12), 
+        plot.title = element_text(size = 16, family = "Noto Sans", face = "bold"),
+        text = element_text(family = "Noto Sans"),
+        panel.background = element_rect(fill = NA))
+
+# Combined plot ####
+Mark_recapture_summary <- ggarrange(Floreana18 + rremove("xlab"),
+                                       Isabela18 + rremove("xlab"),
+                                    Santa_cruz18 + rremove("xlab"),
+                                    Floreana19 + rremove("xlab"),
+                                    Isabela19 + rremove("xlab"),
+                                    Santa_cruz19 + rremove("xlab"),
+                                       labels = c("A", "B", "C",
+                                                  "D", "E", "F"),
+                                       ncol = 3,
+                                       nrow = 2) + 
+  theme(text = element_text(family = "Noto Sans"))
+
+annotate_figure(Mark_recapture_summary,
+                bottom = text_grob("Mericarp Size", just = "centre", face = "bold", family = "Noto Sans", size = 14))
+
+                
