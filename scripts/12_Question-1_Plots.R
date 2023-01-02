734a87c69eab00d4a7b4fc16646df9d03ffda015
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2023-01-02 17:27:27 -0500

    Created figures per trait

--- scripts/12_Question-1_Plots.R
+++ scripts/12_Question-1_Plots.R
@@ -10,7 +10,7 @@
 # First, we set up the theme and functions for each plot.
 # 
 # The figure is a 1:1 ratio line comparing eaten and uneaten mericarps. Per traits. 
-
+# CHECK LOWER SPINES METHODS HOW TO SUMMARIZE
 
 # Plot theme and function ####
 ## Theme ####
@@ -25,28 +25,29 @@ plot_theme <-     theme(axis.line = element_line(linetype = "solid", size = 1.5)
                         legend.title = element_text(size = 14, face = "bold"),
                         legend.position = "right",
                         panel.background = element_rect(fill = NA),
-                        legend.background = element_rect(fill = NA, size = 0))
+                        legend.background = element_rect(fill = NA, size = 0)
+                        ) 
 
 
 ## Functions ####
 # This function is for the plots per trait grouped by island.
 
-island_fig <- function(dataset, x, y, title)
+island_fig <- function(dataset, x, y, title, subtitle)
   {ggplot(dataset, aes(x = x, y = y, colour = island)) + #For the function to work the colors need to be defined here.
   geom_point(shape = "circle", size = 3.5) + #Type of plot
-  scale_color_manual(                        
-    values = c(Baltra = "#E69F00",
-               Espanola = "#56B4E9",
-               Floreana = "#009E73",
-               Isabela = "#0072B2",
-               San.Cristobal = "#D55E00",
-               Santa.Cruz = "#CC79A7",
-               Seymour.Norte = "#666666")) + # Set colors
+  scale_color_manual(
+    values = c("#D55E00",
+               "#E69F00",
+               "#009E73",
+               "#0072B2",
+               "#56B4E9",
+               "#CC79A7",
+               "#666666")) + # Set colors
   labs(
     x = "Eaten",
     y = "Uneaten",
     title = title,
-    subtitle = "Means per island",
+    subtitle = subtitle,
     color = "Islands"
   ) +
   plot_theme +
@@ -56,8 +57,105 @@ island_fig <- function(dataset, x, y, title)
 # Plots ####
 ## Length ####
 
-island_fig(length_means_island,
-           length_means_island$length_mean_1,
-           length_means_island$length_mean_0,
-           "Mericarp Length (mm)")
+island_fig(length_means_island, #Dataset
+           length_means_island$length_mean_1, # x label (eaten)
+           length_means_island$length_mean_0, # y label (uneaten)
+           "Mericarp Length (mm)", # Title
+           "Means per island" # Subtitle (Island or Populations)
+           )
+
+island_fig(length_means_pop,
+           length_means_pop$length_mean_1,
+           length_means_pop$length_mean_0,
+           "Mericarp Length (mm)",
+           "Means per populations"
+           )
+
+## Width ####
+
+island_fig(width_means_island,
+           width_means_island$width_mean_1,
+           width_means_island$width_mean_0,
+           "Mericarp Width (mm)",
+           "Means per island"
+)
+
+
+island_fig(width_means_pop,
+           width_means_pop$width_mean_1,
+           width_means_pop$width_mean_0,
+           "Mericarp Width (mm)",
+           "Means per populations"
+)
+
+
+## Depth ####
+
+island_fig(depth_means_island,
+           depth_means_island$depth_mean_1,
+           depth_means_island$depth_mean_0,
+           "Mericarp Depth (mm)",
+           "Means per island"
+)
+
+island_fig(depth_means_pop,
+           depth_means_pop$depth_mean_1,
+           depth_means_pop$depth_mean_0,
+           "Mericarp Depth (mm)",
+           "Means per populations"
+)
+
+
+## Longest Spine ####
+
+island_fig(longest_spine_means_island,
+           longest_spine_means_island$longest_spine_mean_1,
+           longest_spine_means_island$longest_spine_mean_0,
+           "Mericarp longest spine (mm)",
+           "Means per island"
+)
+
+island_fig(longest_spine_means_pop,
+           longest_spine_means_pop$longest_spine_mean_1,
+           longest_spine_means_pop$longest_spine_mean_0,
+           "Mericarp longest spine (mm)",
+           "Means per populations"
+)
+
+## Spine tip distance ####
+
+island_fig(spine_tip_distance_means_island,
+           spine_tip_distance_means_island$spine_tip_distance_mean_1,
+           spine_tip_distance_means_island$spine_tip_distance_mean_0,
+           "Mericarp Spine Distance (mm)",
+           "Means per island"
+)
+
+island_fig(spine_tip_distance_means_pop,
+           spine_tip_distance_means_pop$spine_tip_distance_mean_1,
+           spine_tip_distance_means_pop$spine_tip_distance_mean_0,
+           "Mericarp Spine Distance (mm)",
+           "Means per populations"
+)
+
+## Spine position ####
+
+island_fig(spine_position_means_island,
+           spine_position_means_island$spine_position_mean_1,
+           spine_position_means_island$spine_position_mean_0,
+           "Mericarp Spine Position",
+           "Means per island"
+)
+
+island_fig(spine_position_means_pop,
+           spine_position_means_pop$spine_position_mean_1,
+           spine_position_means_pop$spine_position_mean_0,
+           "Mericarp Spine Position",
+           "Means per populations"
+)
+
+# Spine position was taken as a factor, so I am not sure if it should be
+# summarized this way.
+# 
+# 
 
