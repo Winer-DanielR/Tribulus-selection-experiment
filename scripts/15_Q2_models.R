053b528fe226c6df19eef26e8f8e00e6f61bafc8
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2023-01-25 16:50:17 -0500

    Question's 2 models per trait.

--- scripts/15_Q2_models.R
+++ scripts/15_Q2_models.R
@@ -21,6 +21,16 @@ width_pop <- read_csv("~/Vault of Ideas/20 - 29 Tribulus Research/24 Chapter. Tr
 lower_spine_pop <- read_csv("~/Vault of Ideas/20 - 29 Tribulus Research/24 Chapter. Tribulus natural selection experiment/24.03 R code/Tribulus Selection experiment/Data/Processed/Questions_1-3_trait_datasets/lower_spines_pop.csv")
 spine_position_pop <- read_csv("~/Vault of Ideas/20 - 29 Tribulus Research/24 Chapter. Tribulus natural selection experiment/24.03 R code/Tribulus Selection experiment/Data/Processed/Questions_1-3_trait_datasets/spine_position_pop.csv")
 
+## Populations per year ####
+## This could give us more observations per population
+depth <- read_csv("~/Vault of Ideas/20 - 29 Tribulus Research/24 Chapter. Tribulus natural selection experiment/24.03 R code/Tribulus Selection experiment/Data/Processed/Point in time means per year/depth_mean_year.csv")
+length <- read_csv("~/Vault of Ideas/20 - 29 Tribulus Research/24 Chapter. Tribulus natural selection experiment/24.03 R code/Tribulus Selection experiment/Data/Processed/Point in time means per year/length_mean_year.csv")
+longest_spine <- read_csv("~/Vault of Ideas/20 - 29 Tribulus Research/24 Chapter. Tribulus natural selection experiment/24.03 R code/Tribulus Selection experiment/Data/Processed/Point in time means per year/longest_spine_mean_year.csv")
+tip_distance <- read_csv("~/Vault of Ideas/20 - 29 Tribulus Research/24 Chapter. Tribulus natural selection experiment/24.03 R code/Tribulus Selection experiment/Data/Processed/Point in time means per year/tip_distance_mean_year.csv")
+width <- read_csv("~/Vault of Ideas/20 - 29 Tribulus Research/24 Chapter. Tribulus natural selection experiment/24.03 R code/Tribulus Selection experiment/Data/Processed/Point in time means per year/width_mean_year.csv")
+lower_spine <- read_csv("~/Vault of Ideas/20 - 29 Tribulus Research/24 Chapter. Tribulus natural selection experiment/24.03 R code/Tribulus Selection experiment/Data/Processed/Point in time means per year/lower_spines_year.csv")
+spine_position <- read_csv("~/Vault of Ideas/20 - 29 Tribulus Research/24 Chapter. Tribulus natural selection experiment/24.03 R code/Tribulus Selection experiment/Data/Processed/Point in time means per year/spine_position_year.csv")
+
 
 # Data preparation ####
 # Making char into factors (island, populations)
@@ -34,6 +44,15 @@ spine_position_pop <- spine_position_pop %>% mutate_at(vars(island, population, 
 tip_distance_pop <- tip_distance_pop %>% mutate_at(vars(island, population), list(factor))
 width_pop <- width_pop %>% mutate_at(vars(island, population), list(factor))
 
+### Populations per year ####
+depth <- depth %>% mutate_at(vars(island, population), list(factor))
+length <- length %>% mutate_at(vars(island, population), list(factor))
+longest_spine  <- longest_spine %>% mutate_at(vars(island, population), list(factor))
+lower_spine <- lower_spine %>% mutate_at(vars(island, population, lower_spine), list(factor))
+spine_position <- spine_position %>% mutate_at(vars(island, population, spine_position), list(factor))
+tip_distance <- tip_distance %>% mutate_at(vars(island, population), list(factor))
+width <- width %>% mutate_at(vars(island, population), list(factor))
+
 
 # Models ####
 # The structure of this model is to test the effect of mean trait values
@@ -41,24 +60,171 @@ width_pop <- width_pop %>% mutate_at(vars(island, population), list(factor))
 # mean trait value ~ mericarp selection
 ## Depth ####
 # Remove NAs
+hist(depth$mean_all, breaks = 30)
 depth_pop <- na.omit(depth_pop)
 
 depth_m2 <- glmmTMB(mean_all ~ S_depth + 
-                      #(1|island),
                       (1|island/population),
                     data = depth_pop,
                     REML = F)
 # I am not sure if this makes sense using this database because each 
 # observation is for a population, there are not various population obs
-
+depth_m3 <- glmmTMB(mean_all ~ S_depth + 
+                      (1|island/population),
+                    data = depth,
+                    REML = F)
 
 ### Model Diagnostics ####
 # Residual histograms
 diagnostic(resid(depth_m2))
-
+diagnostic(resid(depth_m3))
 # DHARMa
 testResiduals(depth_m2)
-
+testResiduals(depth_m3)
 ### Results ####
 summary(depth_m2)
+summary(depth_m3)
 Anova(depth_m2)
+Anova(depth_m3)
+
+# I think is better to use the year dataset because there is at least 3-5 observations per population in this case
+# this justifies using the island nested in population model
+# I will use that dataset for the following models.
+# 
+## Length ####
+hist(length$mean_all, breaks = 30)
+
+length_m2 <- glmmTMB(mean_all ~ S_length + 
+                      (1|island/population),
+                    data = length,
+                    REML = F)
+
+### Model Diagnostics ####
+# Residual histograms
+diagnostic(resid(length_m2))
+
+# DHARMa
+testResiduals(length_m2)
+hist(resid(length_m2), breaks = 50)
+# I think I need to leave it because we have so few observations.
+
+### Results ####
+summary(length_m2)
+Anova(length_m2)
+
+## Longest spine ####
+
+longest_spine_m2 <- glmmTMB(mean_all ~ S_longest_spine + 
+                       (1|island/population),
+                     data = longest_spine,
+                     REML = F)
+
+### Model Diagnostics ####
+# Residual histograms
+diagnostic(resid(longest_spine_m2))
+
+### Data adjustment Longest spine ####
+# Remove earlier years, because there is an difference on how the trait
+# was measured
+longest_spine_filter <- filter(longest_spine, !year == "2015")
+longest_spine_filter <- filter(longest_spine_filter, !year == "2016")
+
+### Filter model ####
+longest_spine_m3 <- glmmTMB(mean_all ~ S_longest_spine + 
+                              (1|island/population),
+                            data = longest_spine_filter,
+                            REML = F)
+
+### Model diagnostics ####
+diagnostic(resid(longest_spine_m3))
+# DHARMa
+testResiduals(longest_spine_m3)
+
+### Results ####
+summary(longest_spine_m3)
+Anova(longest_spine_m3)
+
+## Lower spines ####
+lower_spine_m2 <- glmmTMB(freq_all ~ S_lower_spine + 
+                       (1|island/population),
+                     data = lower_spine,
+                     REML = F)
+
+### Model Diagnostics ####
+# Residual histograms
+diagnostic(resid(lower_spine_m2))
+# DHARMa
+testResiduals(lower_spine_m2)
+# I think this model needs another tpye of distribution but I need to 
+# check with someone else.
+# In fact this is something that I need to check with either Andrew or
+# Marc
+
+### Results ####
+summary(lower_spine_m2)
+Anova(lower_spine_m2)
+
+## Spine Position ####
+hist(spine_position$freq_all, breaks = 30)
+# I think this is a poisson distribution?
+#
+spine_position_m2 <- glmmTMB(freq_all ~ S_spine_position + 
+                            (1|island/population),
+                          data = spine_position,
+                          REML = F)
+
+#
+
+### Model Diagnostics ####
+# Residual histograms
+diagnostic(resid(spine_position_m2))
+# DHARMa
+testResiduals(spine_position_m2)
+# I think this model needs another tpye of distribution but I need to 
+# check with someone else.
+# In fact this is something that I need to check with either Andrew or
+# Marc
+
+### Results ####
+summary(spine_position_m2)
+Anova(spine_position_m2)
+
+## Tip distance ####
+hist(tip_distance$mean_all, breaks = 30)
+
+tip_distance_m2 <- glmmTMB(mean_all ~ S_spine_tip_distance + 
+                       (1|island/population),
+                     data = tip_distance,
+                     REML = F)
+
+### Model Diagnostics ####
+# Residual histograms
+diagnostic(resid(tip_distance_m2))
+
+# DHARMa
+testResiduals(tip_distance_m2)
+
+### Results ####
+summary(tip_distance_m2)
+Anova(tip_distance_m2)
+
+## Width ####
+hist(width$mean_all, breaks = 30)
+
+width_m2 <- glmmTMB(mean_all ~ S_width + 
+                       (1|island/population),
+                     data = width,
+                     REML = F)
+
+### Model Diagnostics ####
+# Residual histograms
+diagnostic(resid(width_m2))
+
+# DHARMa
+testResiduals(width_m2)
+hist(resid(width_m2), breaks = 50)
+# I think I need to leave it because we have so few observations.
+
+### Results ####
+summary(width_m2)
+Anova(width_m2)
