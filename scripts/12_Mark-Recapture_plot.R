72e55ad3adb6899b4a835347b4c59bb99a2a6fc2
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2023-02-24 14:44:51 -0500

    Updated data and created ternary plots

--- scripts/12_Mark-Recapture_plot.R
+++ scripts/12_Mark-Recapture_plot.R
@@ -1,6 +1,7 @@
 # This script is to arrange and create the figure for question 4
-# The figure would be a bar plot with the proportios of eaten mericarps
+# The figure would be a bar plot with the proportions of eaten mericarps
 # per treatments.
+# Also, create a ternary plot.
 # 
 # First I will upload and merge the datasets
 # 
@@ -41,6 +42,64 @@ Isabela_MR <- select(Isabela_MR, c(2:10, 18:21))
 Cruz_MR <- select(Cruz_MR, c(2:10, 18:21))
 # This selects the columns needed for the plot
 
+# Create the Survival column ####
+# Survival is a new column where we put together the three possible outputs of
+# the experiment: Present-uneaten, present-eaten, missing mericarps.
+
+# I also create a new column named Categories, were I unite all the size and
+# spine treatments.
+
+# Finally, I will use the time column too for these plots.
+
+## Floreana ####
+## I am creating two columns one for Bird survival and one for insect survival
+## I would like to explore insect predation even if is only a few mericarps.
+
+Floreana_MR$Bird_Survival <- (Floreana_MR$Present + Floreana_MR$Eaten_Birds)
+Floreana_MR$Insect_Survival <- (Floreana_MR$Present + Floreana_MR$Eaten_Insects)
+
+# This creates two new columns that adds the values of Present and Eaten columns
+# This means that:
+# 2s are present eaten mericarps
+# 1s are present uneaten 
+# 0s are missing mericarps.
+
+Floreana_MR$Bird_Survival[is.na(Floreana_MR$Bird_Survival)] <- 0
+Floreana_MR$Insect_Survival[is.na(Floreana_MR$Insect_Survival)] <- 0
+
+# This transforms NAs into 0s for the new survival columns
+
+Floreana_MR$Categories <- paste(Floreana_MR$size, Floreana_MR$treatment, sep = "_")
+Floreana_MR$Categories <- sub(" ", "_", Floreana_MR$Categories)
+
+
+# This creates a new column that combines size and treatments
+
+## Isabela ####
+Isabela_MR$Bird_Survival <- (Isabela_MR$Present + Isabela_MR$Eaten_Birds)
+Isabela_MR$Insect_Survival <- (Isabela_MR$Present + Isabela_MR$Eaten_Insects)
+
+Isabela_MR$Bird_Survival[is.na(Isabela_MR$Bird_Survival)] <- 0
+Isabela_MR$Insect_Survival[is.na(Isabela_MR$Insect_Survival)] <- 0
+
+Isabela_MR$Categories <- paste(Isabela_MR$size, Isabela_MR$treatment, sep = "_")
+Isabela_MR$Categories <- sub(" ", "_", Isabela_MR$Categories)
+
+
+## Santa Cruz ####
+Cruz_MR$Bird_Survival <- (Cruz_MR$Present + Cruz_MR$Eaten_Birds)
+Cruz_MR$Insect_Survival <- (Cruz_MR$Present + Cruz_MR$Eaten_Insects)
+
+Cruz_MR$Bird_Survival[is.na(Cruz_MR$Bird_Survival)] <- 0
+Cruz_MR$Insect_Survival[is.na(Cruz_MR$Insect_Survival)] <- 0
+
+Cruz_MR$Categories <- paste(Cruz_MR$size, Cruz_MR$treatment, sep = "_")
+Cruz_MR$Categories <- sub(" ", "_", Cruz_MR$Categories)
+
+# In this way we have the categories for treatments,
+# outputs of survival and time for the plots.
+
+# Data transformation ####
 # Transform some columns into factors for the plot
 Floreana_MR <- Floreana_MR %>% mutate_at(vars(year,
                                               time,
@@ -49,6 +108,9 @@ Floreana_MR <- Floreana_MR %>% mutate_at(vars(year,
                                               size,
                                               color,
                                               mark_position,
+                                              Bird_Survival,
+                                              Insect_Survival,
+                                              Categories,
                                               plate), list(factor))
 
 
@@ -59,6 +121,9 @@ Isabela_MR <- Isabela_MR %>% mutate_at(vars(year,
                                               size,
                                               color,
                                               mark_position,
+                                            Bird_Survival,
+                                            Insect_Survival,
+                                            Categories,
                                               plate), list(factor))
 
 
@@ -69,6 +134,9 @@ Cruz_MR <- Cruz_MR %>% mutate_at(vars(year,
                                               size,
                                               color,
                                               mark_position,
+                                      Bird_Survival,
+                                      Insect_Survival,
+                                      Categories,
                                               plate), list(factor))
 
 
@@ -78,110 +146,96 @@ str(Cruz_MR)
 # Transform treatments into factors
 
 
-# Extract the frequencies of present mericarps by size and treatment
-# Grouped by year, time and island
+# Counting and estimating frequencies ####
+# For the ternary plot I need to group counts into the categories.
+# They need to add up to 100, across time.
+# Example:
+# LS1 = LS1PU + LS1PE + LS1M = 100
+# Where, PU, PE and M is Survival and LS is the category in question.
+# So, for time1 all of them are present, so it should be (100, 0, 0).
 
 ## Floreana ####
-Floreana_present <- group_by(Floreana_MR, year, time, island, treatment, size)
-#Floreana_present <- filter(Floreana_present, !time =="0")
-# Perhaps the proportions are better estimated if I take out time 0 which is an absolute.
+Floreana_ternary <- group_by(Floreana_MR, time, Categories, Bird_Survival)
 
-Floreana_present <- dplyr::count(Floreana_present, Present)
-# This counts present mericaprs
+Floreana_ternary <- dplyr::count(Floreana_ternary, Bird_Survival)
+# This counts present mericarps by survival when grouped by time and categories
+# NOTE: Grouping them like this joins 2018 and 2019 experiments!
 
-Floreana_present <- Floreana_present %>%  
-  group_by(year, time, Present) %>% mutate(freq_present = n/sum(n))
+Floreana_ternary <- Floreana_ternary %>%  
+  group_by(time, Categories) %>% mutate(freq_cat = n/sum(n)*100)
+# This estimates the percentage of the counts.
 
-Floreana_present$Presence <- ifelse(Floreana_present$Present == "1","Present","Missing")
+# I think I need to pivot the table into something closer to the sum that
+# Andrew told me.
+Floreana_ternary_pivot <- pivot_wider(Floreana_ternary, names_from = Bird_Survival,
+                               values_from = c(4:5))
+# I think this is what I need all the values are set in the way we need.
+# Need to replace NAs for 0s
+Floreana_ternary_pivot[is.na(Floreana_ternary_pivot)] <- 0
 
+# I also need to rename the columns because the survival numbers are not that intutitive
 
-## Isabela ####
-Isabela_present <- group_by(Isabela_MR, year, time, island, treatment, size)
-#Isabela_present <- filter(Isabela_present, !time =="0")
-# Perhaps the proportions are better estimated if I take out time 0 which is an absolute.
-
-Isabela_present <- dplyr::count(Isabela_present, Present)
-# This counts present mericaprs
-
-Isabela_present <- Isabela_present %>%  
-  group_by(year, time, Present) %>% mutate(freq_present = n/sum(n))
-
-Isabela_present$Presence <- ifelse(Isabela_present$Present == "1","Present","Missing")
-
-
-## Santa Cruz ####
-Cruz_present <- group_by(Cruz_MR, year, time, island, treatment, size)
-#Cruz_present <- filter(Cruz_present, !time =="0")
-# Perhaps the proportions are better estimated if I take out time 0 which is an absolute.
-
-Cruz_present <- dplyr::count(Cruz_present, Present)
-# This counts present mericarps
-
-Cruz_present <- Cruz_present %>%  
-  group_by(year, time, Present) %>% mutate(freq_present = n/sum(n))
-
-Cruz_present$Presence <- ifelse(Cruz_present$Present == "1","Present","Missing")
-
-# Plots ####
-
-Floreana_present %>%
-  filter(!(time %in% c("0", "4"))) %>%
-  ggplot() +
-  aes(x = treatment, y = freq_present, fill = Presence) +
-  geom_col(position = position_dodge()
-    ) +
-  #geom_text(aes(label = paste(freq_present, "%")), vjust = -0.25) +
-  scale_fill_brewer(palette = "Dark2", direction = 1) +
-  labs(
-    x = "Size and Treatment",
-    y = "Frequency %",
-    title = "Floreana",
-    subtitle = "Joined times 1, 2 and 3",
-    fill = "Mericarps"
-  ) +
-  theme_minimal() +
-  facet_wrap(~size, strip.position = "bottom", scales = "free_x") +
-  theme(panel.spacing = unit(0, "lines"),
-        strip.background = element_blank(),
-        strip.placement = "outside")
-
-## Isabela ####
-
-Isabela_present %>%
-  filter(!(time %in% c("0", "4"))) %>%
-  ggplot() +
-  aes(x = treatment, y = freq_present, fill = Presence) +
-  geom_col(position = position_dodge()) +
-  scale_fill_brewer(palette = "Dark2", direction = 1) +
-  labs(
-    x = "Size and Treatment",
-    y = "Frequency %",
-    title = "Isabela",
-    fill = "Mericarps"
-  ) +
-  theme_minimal() +
-  facet_wrap(~size, strip.position = "bottom", scales = "free_x") +
-  theme(panel.spacing = unit(0, "lines"),
-        strip.background = element_blank(),
-        strip.placement = "outside")
+Floreana_ternary_pivot <- rename(Floreana_ternary_pivot,
+                                 n_uneaten = n_1,
+                                 n_eaten = n_2,
+                                 n_missing = n_0,
+                                 Uneaten_freq = freq_cat_1,
+                                 Eaten_freq = freq_cat_2,
+                                 Missing_freq = freq_cat_0)
 
 ## Santa Cruz ####
+Cruz_ternary <- group_by(Cruz_MR, time, Categories, Bird_Survival)
 
-Cruz_present %>%
-  filter(!(time %in% c("0", "4"))) %>%
-  ggplot() +
-  aes(x = treatment, y = freq_present, fill = Presence) +
-  geom_col(position = position_dodge()) +
-  scale_fill_brewer(palette = "Dark2", direction = 1) +
-  labs(
-    x = "Size and Treatments",
-    y = "Frequency %",
-    title = "Santa Cruz",
-    fill = "Mericarps"
-  ) +
-  theme_minimal() +
-  facet_wrap(~size, strip.position = "bottom", scales = "free_x") +
-  theme(panel.spacing = unit(0, "lines"),
-        strip.background = element_blank(),
-        strip.placement = "outside")
+Cruz_ternary <- dplyr::count(Cruz_ternary, Bird_Survival)
 
+Cruz_ternary <- Cruz_ternary %>%  
+  group_by(time, Categories) %>% mutate(freq_cat = n/sum(n)*100)
+
+Cruz_ternary_pivot <- pivot_wider(Cruz_ternary, names_from = Bird_Survival,
+                                      values_from = c(4:5))
+
+Cruz_ternary_pivot[is.na(Cruz_ternary_pivot)] <- 0
+
+Cruz_ternary_pivot <- rename(Cruz_ternary_pivot,
+                                 n_uneaten = n_1,
+                                 n_eaten = n_2,
+                                 n_missing = n_0,
+                                 Uneaten_freq = freq_cat_1,
+                                 Eaten_freq = freq_cat_2,
+                                 Missing_freq = freq_cat_0)
+
+## Isabela ####
+Isabela_ternary <- group_by(Isabela_MR, time, Categories, Bird_Survival)
+
+Isabela_ternary <- dplyr::count(Isabela_ternary, Bird_Survival)
+
+Isabela_ternary <- Isabela_ternary %>%  
+  group_by(time, Categories) %>% mutate(freq_cat = n/sum(n)*100)
+
+Isabela_ternary_pivot <- pivot_wider(Isabela_ternary, names_from = Bird_Survival,
+                                     values_from = c(4:5))
+
+Isabela_ternary_pivot[is.na(Isabela_ternary_pivot)] <- 0
+
+Isabela_ternary_pivot <- rename(Isabela_ternary_pivot,
+                                n_uneaten = n_1,
+                                n_eaten = n_2,
+                                n_missing = n_0,
+                                Uneaten_freq = freq_cat_1,
+                                Eaten_freq = freq_cat_2,
+                                Missing_freq = freq_cat_0)
+
+
+# Now, this pivoted dataframe is the one that I need for the ternary plot per
+# category and time.
+
+# Ternary plot ####
+ggtern(data = Floreana_ternary_pivot,
+       aes(Missing_freq,
+           Uneaten_freq,
+           Eaten_freq, color = time,
+           shape = Categories)) +
+  geom_point() + labs(title = "Floreana") +
+  xlab("Missing") +
+  ylab("Uneaten") +
+  zlab("Eaten") + theme_showarrows()
