9a601800c2865de2e4683f28036a6686496675e3
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2023-05-01 15:03:43 -0500

    Q4 model analysis using the ternary plots

---
+++ scripts/15_Mark-Recapture_models.R
@@ -0,0 +1,37 @@
+# Date: 5/1/2023
+# Script by: Daniel Reyes
+# Question 4 Model analysis
+# Goal: Model analysis for question 4 using the mark recapture data.
+# I will use the ternary dataset, which has the estimate frequencies of eaten, uneaten, and missing
+# mericarps per category and also per island and time of the experiment.
+# This is a tentative analysis, perhaps we could use something else, like the PC scores.
+
+# Loading ternary dataset ####
+MR_ternary <- read_csv("~/Vault of Ideas/20 - 29 Tribulus Research/24 Chapter. Tribulus natural selection experiment/24.03 R code/Tribulus Selection experiment/Data/Processed/Question 4/Island_ternary_dataset.csv")
+
+# Data preparation ####
+# Categories <- factor
+# Time <- factor
+# Island <- factor
+
+str(MR_ternary)
+
+MR_ternary <- MR_ternary %>% mutate_at(vars(time,
+                                              Categories,
+                                              Island), list(factor))
+
+# The model structure using this dataset would be to predict the eaten, uneaten and missing mericarp frequency
+# based on categories, time, and island as a random factor. 
+# Since time 0 and 4 are mostly absolute values, I think I would filter them out.
+
+MR_ternary_filter <- filter(MR_ternary, !time == "0")
+MR_ternary_filter <- filter(MR_ternary_filter, !time == "4")
+
+# Models ####
+## Eaten mericarps ####
+
+eaten_Q4 <- glmmTMB(Eaten_freq ~ Categories + time + (1|Island),
+                    data = MR_ternary_filter,
+                    REML = F)
+
+## Model diagnostics ####
