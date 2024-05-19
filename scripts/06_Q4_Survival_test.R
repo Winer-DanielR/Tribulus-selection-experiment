807baee35f0e5c4f03d3f7c7ee4811a1c0dc9826
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2024-05-19 13:00:53 -0500

    Updated script with more comments, plot theme and color palette.

--- scripts/06_Q4_Survival_test.R
+++ scripts/06_Q4_Survival_test.R
@@ -1,15 +1,24 @@
 # Survival Analysis test ####
 # Code by: Daniel Reyes Corral
+# Date: May 2024
 # 
 # Description ####
-# Survival analysis using the mark recapture experiment.
+# Survival analysis using the mark recapture experiment. Using eaten or missing
+# mericarps as the survival events and days as the time of the experiment
+# The experiment had various categories of size and spine treatments for mericarps
+# and the survival of the individual mericarps were tracked over a year in 2018 and over
+# 3 months in 2019.
 # 
+# Potential analysis ####
+# Using a combination of categories and groups to show the results of the experiment
+# 
+#  
 # # 1. The survival analysis can be done per island. Comparing eaten mericarps
 # and missing mericarps results.
 # 
 # 2. Filter categories to compare between years.
 # 
-# 3. Analysis of only 2018 and all categories.
+# 3. Analysis of only 2018 to compare lower and upper spine treatments.
 # 
 # 4. Use time in days, rather than time 1, 2 or 3.
 
@@ -27,17 +36,6 @@ Isabela_2019 <- read_csv("~/Thesis reasearch/20 - 29 Tribulus Research/24 Chapte
 ## Santa Cruz ####
 Cruz_2018 <- read_csv("~/Thesis reasearch/20 - 29 Tribulus Research/24 Chapter. Tribulus natural selection experiment/24.03 R code/Tribulus Selection experiment/Data/Raw/Santa Cruz 2018.csv")
 Cruz_2019 <- read_csv("~/Thesis reasearch/20 - 29 Tribulus Research/24 Chapter. Tribulus natural selection experiment/24.03 R code/Tribulus Selection experiment/Data/Raw/Santa Cruz 2019.csv")
-
-# Checking the column type for each dataset
-# Floreana
-str(Floreana_2018)
-str(Floreana_2019)
-# Isabela
-str(Isabela_2018)
-str(Isabela_2019)
-# Santa Cruz
-str(Cruz_2018)
-str(Cruz_2019)
 
 # Combining datasets per year ####
 # Merging both years mericarps
@@ -61,37 +59,109 @@ Isabela_MR$Categories <- sub(" ", "_", Isabela_MR$Categories)
 Cruz_MR$Categories <- paste(Cruz_MR$size, Cruz_MR$treatment, sep = "_")
 Cruz_MR$Categories <- sub(" ", "_", Cruz_MR$Categories)
 
-# Selecting columns for model analysis ####
+## Selecting columns for model analysis ####
 
 Floreana_survival <- select(Floreana_MR, c(2:10, 18:21, 27))
 Isabela_survival <- select(Isabela_MR, c(2:10, 18:21, 26))
 Cruz_survival <- select(Cruz_MR, c(2:10, 18:21, 27))
 
+# Datasets for analysis ####
+## Single general dataset ####
 # Combining island datasets into a single dataset for survival analysis
-# (this is preliminary)
+# 
 
 MR_survival <- bind_rows(Floreana_survival,
                          Isabela_survival,
                          Cruz_survival)
 MR_survival <- as_tibble(MR_survival)
 
-## Filter Survival Dataset ####
+## Dataset for upper an lower spines treatment ####
 # Filter survival dataset to test all categories
 MR_survival_2018 <- filter(MR_survival, year == "2018")
 target1 <- c("Lower spines", "Upper spines")
 MR_survival_2018 <- filter(MR_survival_2018, treatment %in% target1)
 
+## Dataset combining 2018 and 2019 experiments ####
 # Filter dataset to test 2018 and 2019 categories
 target <- c("All spines", "No spines")
 MR_survival_filter <- filter(MR_survival, treatment %in% target)
+
+## Plot preparation ####
+## Color scales and plot themes
+colors <- c("#0072B2",
+            "#009E73",
+            "#D55E00",
+            "#CC79A7",
+            "#F0E442",
+            "#000000",
+            "#56B4E9",
+            "#E69F00",
+            "#999999"
+            )
+
+## Plot theme
+plot_theme <-     theme(axis.line = element_line(linetype = "solid", size = 1), 
+                        axis.title = element_text(size = 14, 
+                                                  face = "bold"
+                        ),
+                        axis.text = element_text(size = 12, face = "bold"), 
+                        axis.text.x = element_text(size = 12), 
+                        plot.title = element_text(size = 16, face = "bold", hjust = 0),
+                        text = element_text(family = "Noto Sans"),
+                        legend.text = element_text(size = 11), 
+                        legend.title = element_text(size = 12, face = "bold"),
+                        legend.position = "right",
+                        panel.background = element_rect(fill = NA),
+                        legend.background = element_rect(fill = NA, size = 0),
+                        strip.text = element_text(size = 10, face = "bold"),
+                        strip.background = element_blank(),
+                        panel.spacing = unit(1, "cm"))
+
+
+## Island analysis ####
+## We can use the respective Mark Recapture datasets above per island.
 
 # Survival Analysis ####
 # Run the Kaplan-Meier estimator with the
 # surfit() and Surv() functions:
 
 ## KM estimator ####
-# This KM estimator is based on the single survival dataset above.
+# Most of the KM estimates will be done using the
+# general survival dataset above.
 # I will use it to compare multiple variables.
+
+### KM estimates per island and survival plots ####
+### Here I am generating KM estimates and plots per island
+### using the All spines and No spines treatments.
+#### KM Floreana ####
+KM_Floreana_days_eaten <- survfit(Surv(days_pass, Eaten_Birds) ~ size + treatment,
+                                  data = Floreana_MR)
+# Using eaten mericarps as survival estimates
+
+KM_Floreana_days_missing <- survfit(Surv(days_pass, Present) ~ size + treatment,
+                                    data = Floreana_MR)
+
+#### Floreana Survival plot ####
+ggsurvplot(KM_Floreana_days_missing, legend = "right",
+           surv.median.line = "hv",
+           pval = T,
+           conf.int = T,
+           palette = colors,
+           xlab = "Time (Days)",
+           legend.title = "Mericarp Categories",
+           legend.labs = c("Large - All spines",
+                           "Large - No spines",
+                           "Small - All spines",
+                           "Small - No spines"),
+           title = "Floreana",
+           ggtheme = plot_theme)
+
+
+ggsurvplot(KM_MR_islands_missing, legend = "right", surv.median.line = "hv",
+           pval = T,
+           conf.int = T,
+           palette = "Okabe-Ito")
+
 
 ### 1. Compare between Time and Days Passed for eaten mericarps ####
 
